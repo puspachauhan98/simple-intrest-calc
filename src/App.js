f35c5c4fedeb5c74edba39817c03fc458da918a9
Puspa Chauhan <puspa@Puspas-MacBook-Air.local>
2024-02-28 13:41:51 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import React, {useState} from 'react';
+import Button from '@mui/material/Button';
 
-function App() {
+
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import './index.css';
+
+const App =()=>{
+  const[p,setP]=useState(0)
+  const[r,setR]=useState(0)
+  const[t,setT]=useState(0)
+  const [si,setSi]=useState(0)
+  const calculateIntrest=()=>{
+    setSi(p*r*t/100)
+  
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="div">
+     
+      <AppBar position="static">
+        <Toolbar>
+         
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Simple Intrest
+          </Typography>
+    
+        </Toolbar>
+      </AppBar>
+      <br></br>
+      <TextField onChange={(event) => setP(event.target.value)}id="outlined-basic" label="Enter Principle" variant="outlined" />
+      <TextField onChange={(event) => setT(event.target.value)}id="outlined-basic" label="Enter Time" variant="outlined" />
+
+      <TextField onChange={(event) => setR(event.target.value)}id="outlined-basic" label="Enter Rate" variant="outlined" />
+
+  <br>
+</br>
+<Button onClick={()=> calculateIntrest()} variant="contained">Calculate</Button>
+<Typography variant="h6" gutterBottom component='div'>
+  Simple Intrest is: {si}
+  </Typography>
+  
+
+      
+    
     </div>
   );
 }
